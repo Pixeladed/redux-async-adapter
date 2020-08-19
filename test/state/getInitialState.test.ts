43477cb19fada8aaff16e1c2127c75ace358891e
Pixeladed <vietminhle98@gmail.com>
2020-08-19 14:34:21 +0800

    test(state): add tests for state functions

---
+++ test/state/getInitialState.test.ts
@@ -0,0 +1,23 @@
+import createAsyncAdapter from 'index';
+import { AsyncState } from 'types';
+
+describe('getInitialState', () => {
+  it('should return the expected state shape', () => {
+    const adapter = createAsyncAdapter();
+    const result = adapter.getInitialState({});
+
+    const stateShape: AsyncState<{}> = {
+      data: {},
+      status: {},
+    };
+    expect(result).toStrictEqual(stateShape);
+  });
+
+  it('should preserve initial data', () => {
+    const adapter = createAsyncAdapter();
+    const initialData = { hello: 'world' };
+    const result = adapter.getInitialState(initialData);
+
+    expect(result.data).toStrictEqual(initialData);
+  });
+});
