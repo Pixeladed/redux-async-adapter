94de5a6b50f4f854061c2a7a72244d541d1d7dab
Pixeladed <vietminhle98@gmail.com>
2020-08-19 13:13:10 +0800

    test(selectors): add selectAllStatuses tests

---
+++ test/selectors/selectAllStatuses.ts
@@ -0,0 +1,59 @@
+import * as selectors from 'selectors';
+import {
+  createAsyncThunk,
+  createReducer,
+  configureStore,
+} from '@reduxjs/toolkit';
+import { getInitialState } from 'state';
+import { handlePending, handleFulfilled, handleRejected } from 'reducers';
+import { AsyncStatus } from 'types';
+import flushPromises from 'flush-promises';
+
+describe('selectAllStatuses', () => {
+  it('should return empty array if no thunks have been dispatched', () => {
+    const store = configureStore({ reducer: () => getInitialState({}) });
+    const statuses = selectors.selectAllStatuses(store.getState());
+    expect(statuses.length).toBe(0);
+  });
+
+  it('should return all statuses of dispatched thunks', async () => {
+    const thunk1 = createAsyncThunk('thunk 1', async () => {});
+    const thunk2 = createAsyncThunk('thunk 2', async () => {});
+    const thunk3 = createAsyncThunk('thunk 3', async () => {});
+
+    const store = configureStore({
+      reducer: createReducer(getInitialState({}), {
+        [thunk1.pending.type]: handlePending(thunk1),
+        [thunk1.fulfilled.type]: handleFulfilled(thunk1),
+        [thunk1.rejected.type]: handleRejected(thunk1),
+        [thunk2.pending.type]: handlePending(thunk2),
+        [thunk2.fulfilled.type]: handleFulfilled(thunk2),
+        [thunk2.rejected.type]: handleRejected(thunk2),
+        [thunk3.pending.type]: handlePending(thunk3),
+        [thunk3.fulfilled.type]: handleFulfilled(thunk3),
+        [thunk3.rejected.type]: handleRejected(thunk3),
+      }),
+    });
+
+    store.dispatch(thunk1());
+    store.dispatch(thunk2());
+    store.dispatch(thunk3());
+    await flushPromises();
+
+    const statuses = selectors.selectAllStatuses(store.getState());
+    const getResult = (name: string): Partial<AsyncStatus> =>
+      expect.objectContaining({
+        name,
+        error: undefined,
+        loaded: true,
+        loading: false,
+      });
+    const result = [
+      getResult(thunk1.typePrefix),
+      getResult(thunk2.typePrefix),
+      getResult(thunk3.typePrefix),
+    ];
+
+    expect(statuses).toStrictEqual(result);
+  });
+});
